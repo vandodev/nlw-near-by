0b9687a4fc722a5eeb8d96682c50511ad5868024
vandodev <vandoaparecido@hotmail.com>
2024-12-10 08:44:01 -0300

    :airplane: :love_hotel: :dollar: Componente button

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -2,12 +2,17 @@
 import { Steps } from "@/components/steps"
 import { Welcome } from "@/components/welcome"
 import { View } from "react-native"
+import { Button } from "@/components/button"
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
+
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   )
 }
