6c8fb1af4cdd2082b8e965c3e2bbb2ca3ecff208
vandodev <vandoaparecido@hotmail.com>
2024-12-09 21:50:39 -0300

    :airplane: :love_hotel: :dollar: Componente de boas vindas

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,10 +1,11 @@
 
+import { Welcome } from "@/components/welcome"
 import { View } from "react-native"
 
 export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
-   
+      <Welcome />
     </View>
   )
 }
