6a63fc1894f74cc7791a8853c0094c34a4a1d672
vandodev <vandoaparecido@hotmail.com>
2024-12-09 22:21:36 -0300

    :airplane: :love_hotel: :dollar: Componente steps

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,4 +1,5 @@
 
+import { Steps } from "@/components/steps"
 import { Welcome } from "@/components/welcome"
 import { View } from "react-native"
 
@@ -6,6 +7,7 @@ export default function Index() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
+      <Steps />
     </View>
   )
 }
