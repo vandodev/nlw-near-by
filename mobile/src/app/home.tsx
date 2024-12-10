d87bf9908130dc6b5a30e074ec7e71ce63523656
vandodev <vandoaparecido@hotmail.com>
2024-12-10 10:29:48 -0300

    :airplane: :love_hotel: :dollar: Buscando categorias da api

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,12 +1,37 @@
-import { View } from "react-native"
+import { View, Alert } from "react-native"
+import { useEffect, useState } from "react"
+import { Categories, CategoriesProps } from "@/components/categories"
 
 import { api } from "@/services/api"
 
 export default function Home() {
+
+    const [categories, setCategories] = useState<CategoriesProps>([])
+    const [category, setCategory] = useState("")
+
+    async function fetchCategories() {
+        try {
+          const { data } = await api.get("/categories")
+          setCategories(data)
+          setCategory(data[0].id)
+        } catch (error) {
+          console.log(error)
+          Alert.alert("Categorias", "Não foi possível carregar as categorias.")
+        }
+    }
+
+    useEffect(() => {
+        fetchCategories()
+    }, [])
+    
   
   return (
     <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
-     
+      <Categories
+        data={categories}
+        onSelect={setCategory}
+        selected={category}
+      />
     </View>
   )
 }
