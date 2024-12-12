a225d33224d827736cfc8ff62010c615be538968
vandodev <vandoaparecido@hotmail.com>
2024-12-12 07:37:53 -0300

    :airplane: :love_hotel: :dollar: Buscando dados da api

--- mobile/src/app/market/[id].tsx
+++ mobile/src/app/market/[id].tsx
@@ -1,8 +1,30 @@
-import { View, StatusBar, Text } from "react-native"
-import { useLocalSearchParams } from "expo-router"
+import { useEffect, useState, useRef } from "react"
+import { View, StatusBar, Text, Alert } from "react-native"
+import { api } from "@/services/api"
+import { router, useLocalSearchParams } from "expo-router"
 
 export default function Market() {
   const params = useLocalSearchParams<{ id: string }>()
+
+  async function fetchMarket() {
+    try {
+      const { data } = await api.get(`/markets/${params.id}`)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Erro", "Não foi possível carregar os dados", [
+        {
+          text: "OK",
+          onPress: () => router.back(),
+        },
+      ])
+    }
+  }
+
+  useEffect(() => {
+    fetchMarket()
+  }, [params.id])
+
   return (
     <View style={{ flex: 1 }}>
       <StatusBar barStyle="light-content" />
