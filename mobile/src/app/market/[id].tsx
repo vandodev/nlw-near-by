423e1d0418b49722ffef136b25eeda8b70f18efe
vandodev <vandoaparecido@hotmail.com>
2024-12-13 08:01:43 -0300

    :airplane: :love_hotel: :dollar: Trabalhando com expo câmera

--- mobile/src/app/market/[id].tsx
+++ mobile/src/app/market/[id].tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState, useRef } from "react"
-import { View, StatusBar, Text, Alert } from "react-native"
+import { View, StatusBar, Text, Modal, Alert } from "react-native"
 import { api } from "@/services/api"
 import { router, useLocalSearchParams, Redirect } from "expo-router"
 import { Loading } from "@/components/loading"
 import { Cover } from "@/components/market/cover"
 import { Details, PropsDetails } from "@/components/market/details"
 import { Coupon } from "@/components/market/coupon"
+import { Button } from "@/components/button"
+import { useCameraPermissions, CameraView } from "expo-camera"
 
 type DataProps = PropsDetails & {
   cover: string
@@ -16,6 +18,8 @@ export default function Market() {
   const [isLoading, setIsLoading] = useState(true)
   const params = useLocalSearchParams<{ id: string }>()
   const [coupon, setCoupon] = useState<string | null>(null)
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false)
+  const [_, requestPermission] = useCameraPermissions()
 
   async function fetchMarket() {
     try {
@@ -30,6 +34,20 @@ export default function Market() {
           onPress: () => router.back(),
         },
       ])
+    }
+  }
+
+  async function handleOpenCamera() {
+    try {
+      const { granted } = await requestPermission()
+      if (!granted) {
+        return Alert.alert("Câmera", "Você precisa habilitar o uso da câmera")
+      }
+      // qrLock.current = false
+      setIsVisibleCameraModal(true)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Câmera", "Não foi possível utilizar a câmera")
     }
   }
 
@@ -51,6 +69,26 @@ export default function Market() {
       <Cover uri={data.cover} />  
       <Details data={data} />  
       {coupon && <Coupon code={coupon} />}
+
+      <View style={{ padding: 32 }}>
+        <Button onPress={handleOpenCamera}>
+          <Button.Title>Ler QR Code</Button.Title>
+        </Button>
+      </View>
+
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <CameraView
+          style={{ flex: 1 }}
+          facing="back"
+        />
+        <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+          <Button
+            onPress={() => setIsVisibleCameraModal(false)}            
+          >
+            <Button.Title>Voltar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   )
 }
--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://192.168.2.104:3333",
+  baseURL: "http://192.168.0.102:3333",
   timeout: 700,
 })
